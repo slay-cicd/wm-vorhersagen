6fb1af3afe41090ffc38894b3f1c1f4a4e6a6119
Jochen <jochen@born.com>
2026-04-12 14:05:01 +0200

    cross-links update

---
+++ _next/static/chunks/app/not-found-22f3ca95f95b988c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[345],{2035:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6235,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[235,878,945,358],()=>s(2035)),_N_E=e.O()}]);
